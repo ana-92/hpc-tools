8cdf980588807d6ca6977d6e9de28fa982cc3870
AnaDev <ana.izaguirrematamoros@gmail.com>
2023-10-05 15:59:00 -0600

    fix: dgesv.c file updated

--- dgesv.c
+++ dgesv.c
@@ -1,3 +1,9 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -6,99 +12,66 @@ void my_dgesv(int N, double *A, double *B) {
     int i, j, k;
     double pivot;
     int *pivots = (int *)malloc(N * sizeof(int));
-    double *P = (double *)malloc(N * N * sizeof(double));
 
-    // Initialize pivot array and the permutation matrix P
     for (i = 0; i < N; i++) {
-        pivots[i] = i ; // Start from 1, not 0
-        for (j = 0; j < N; j++) {
-            if (i == j) {
-                P[i * N + j] = 1.0; // Diagonal elements of P are 1
-            } else {
-                P[i * N + j] = 0.0;
-            }
-        }
+        pivots[i] = i;
     }
 
-    // Forward elimination with partial pivoting
     for (i = 0; i < N; i++) {
-        // Find the pivot element with the largest magnitude
-        pivot = fabs(A[(pivots[i] - 1) * N + i]);
+        pivot = fabs(A[pivots[i] * N + i]);
         int pivot_row = i;
 
         for (j = i + 1; j < N; j++) {
-            double abs_val = fabs(A[(pivots[j] - 1) * N + i]);
+            double abs_val = fabs(A[pivots[j] * N + i]);
             if (abs_val > pivot) {
                 pivot = abs_val;
                 pivot_row = j;
             }
         }
 
-        // Swap row indices in the pivot array
         int temp = pivots[i];
         pivots[i] = pivots[pivot_row];
         pivots[pivot_row] = temp;
 
-        // Update permutation matrix P accordingly
         for (j = 0; j < N; j++) {
-            double temp_p = P[i * N + j];
-            P[i * N + j] = P[pivot_row * N + j];
-            P[pivot_row * N + j] = temp_p;
+            double temp_a = A[pivots[i] * N + j];
+            A[pivots[i] * N + j] = A[pivots[pivot_row] * N + j];
+            A[pivots[pivot_row] * N + j] = temp_a;
         }
 
-        // Divide the current row by the pivot element
-        pivot = A[(pivots[i] - 1) * N + i];
+        // Swap rows in B using the same pivot order
+        for (j = 0; j < N; j++) {
+            double temp_b = B[pivots[i] * N + j];
+            B[pivots[i] * N + j] = B[pivots[pivot_row] * N + j];
+            B[pivots[pivot_row] * N + j] = temp_b;
+        }
+
+        pivot = A[pivots[i] * N + i];
+
         for (j = i; j < N; j++) {
-            A[(pivots[i] - 1) * N + j] /= pivot;
+            A[pivots[i] * N + j] /= pivot;
         }
+
         for (j = 0; j < N; j++) {
-            B[(pivots[i] - 1) * N + j] /= pivot;
+            B[pivots[i] * N + j] /= pivot;
         }
 
-        // Eliminate other rows
-        for (k = i + 1; k < N; k++) {
-            pivot = A[(pivots[k] - 1) * N + i];
-            for (j = i; j < N; j++) {
-                A[(pivots[k] - 1) * N + j] -= pivot * A[(pivots[i] - 1) * N + j];
-            }
-            for (j = 0; j < N; j++) {
-                B[(pivots[k] - 1) * N + j] -= pivot * B[(pivots[i] - 1) * N + j];
-            }
-        }
-    }
 
-    // Back substitution
-    for (i = N - 1; i >= 0; i--) {
-        for (k = i - 1; k >= 0; k--) {
-            pivot = A[(pivots[k] - 1) * N + i];
-            for (j = i; j < N; j++) {
-                A[(pivots[k] - 1) * N + j] -= pivot * A[(pivots[i] - 1) * N + j];
-            }
-            for (j = 0; j < N; j++) {
-                B[(pivots[k] - 1) * N + j] -= pivot * B[(pivots[i] - 1) * N + j];
+        for (k = 0; k < N; k++) {
+            if (k != i) {
+                pivot = A[pivots[k] * N + i];
+                for (j = i; j < N; j++) {
+                    A[pivots[k] * N + j] -= pivot * A[pivots[i] * N + j];
+                }
+                for (j = 0; j < N; j++) {
+                    B[pivots[k] * N + j] -= pivot * B[pivots[i] * N + j];
+                }
             }
         }
     }
 
-    // Apply permutation matrix P to the solution B
-    double *temp_B = (double *)malloc(N * N * sizeof(double));
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            temp_B[i * N + j] = B[(pivots[i] - 1) * N + j];
-        }
-    }
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            B[i * N + j] = 0.0;
-            for (k = 0; k < N; k++) {
-                B[i * N + j] += P[i * N + k] * temp_B[k * N + j];
-            }
-        }
-    }
-
-    // Clean up
     free(pivots);
-    free(P);
-    free(temp_B);
 }
+
+
+
